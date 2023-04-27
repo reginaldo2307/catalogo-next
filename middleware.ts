4e8e963e7ed741131fd07e403d9cdcd0c4397a16
reginaldo2307 <reginaldo1982@hotmail.com>
2023-04-27 14:02:19 -0300

    hostname error

--- middleware.ts
+++ middleware.ts
@@ -16,11 +16,9 @@ export default async function middleware(req: NextRequest) {
   // (in the case of "subdomain-3.localhost:3000", "localhost:3000" is the root URL)
   // process.env.NODE_ENV === "production" indicates that the app is deployed to a production environment
   // process.env.VERCEL === "1" indicates that the app is deployed on Vercel
-  const currentHost =
-    process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
-      ? hostname
-        .replace(`.whaticketlk.online`, "")
-      : hostname.replace(`.localhost:3000`, "");
+  const currentHost = process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
+      ? hostname.replace(`.whaticketlk.online`, "success")
+      : hostname.replace(`.localhost:3000`, "success");
 
 
 
