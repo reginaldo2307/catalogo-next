c91b5c408b8c6293f022fa588c9118f783262519
reginaldo2307 <reginaldo1982@hotmail.com>
2023-04-27 12:57:32 -0300

    hostname

--- middleware.ts
+++ middleware.ts
@@ -19,7 +19,7 @@ export default async function middleware(req: NextRequest) {
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname
-        .replace(`.buildwithnext.com`, "")
+        .replace(`.whaticketlk.online`, "")
       : hostname.replace(`.localhost:3000`, "");
 
 
