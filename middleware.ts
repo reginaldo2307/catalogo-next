4b9018a95c833cbb99eb227fdf047f0f8b0ebcd2
reginaldo2307 <reginaldo1982@hotmail.com>
2023-05-05 15:24:17 -0300

    Update middleware.ts
    Aterando domínio

--- middleware.ts
+++ middleware.ts
@@ -18,7 +18,7 @@ export default async function middleware(req: NextRequest) {
   // process.env.VERCEL === "1" indicates that the app is deployed on Vercel
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
-      ? hostname?.replace(`.whaticketlk.online`, "")
+      ? hostname?.replace(`.vercel.pub`, "")
       : hostname?.replace(`.localhost:3000`, "");
 
 
